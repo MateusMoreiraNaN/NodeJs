4454c9fc0aa08727005bacf014d38c6f5e7d0ba1
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-03-21 19:51:32 -0300

    first commit

--- ApiLogin/src/controller/ApiController.ts
+++ ApiLogin/src/controller/ApiController.ts
@@ -2,6 +2,26 @@ import { Request, Response } from 'express'
 import { Mongoose } from 'mongoose'
 import User from '../models/User'
 
-export const ping = (req: Request, res: Response)=>{
-    res.json({pong: true})
+export const createLogin = async (req: Request, res: Response)=>{
+    let { email, password } = req.body
+
+    if(email){
+        let newLogin = new User()
+
+        newLogin.email = email
+
+        if(password){
+            newLogin.password = password
+        }
+
+        await newLogin.save()
+    }
+
+
+
+    res.json({email, password })
+
+    
+
+    
 }
