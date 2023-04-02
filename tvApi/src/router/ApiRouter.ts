bc327545bebc22cdac26c9c1e80a7c6bb0dbe78b
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-04-02 19:42:51 -0300

    subir

--- tvApi/src/router/ApiRouter.ts
+++ tvApi/src/router/ApiRouter.ts
@@ -1,6 +1,14 @@
 import { Router } from "express"
 
+import * as apiController from '../controllers/apiController'
+
 const router = Router()
+
+router.get('/films', apiController.filmAll)
+router.post('/film', apiController.filmAdd)
+router.put('/film:id', apiController.filmUpdate)
+router.delete('/film:id', apiController.filmRemove)
+
 
 
 export default router
