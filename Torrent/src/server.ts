bde97c67ac343c45dc5da52ae296eadc947dea0b
“MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-04-27 19:37:48 -0300

    save

--- Torrent/src/server.ts
+++ Torrent/src/server.ts
@@ -2,4 +2,26 @@ import express, { Request, Response } from "express";
 import path from "path";
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
+import RouterFilms from './routes/films'
+
+
+dotenv.config()
+
+const server = express()
+
+server.set('view engine', 'ejs')
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.urlencoded({extended: true}))
+server.use(bodyParser.urlencoded({extended: false}))
+server.use(bodyParser.json())
+
+server.use(RouterFilms)
+
+server.use((req: Request, res: Response)=>{
+    res.status(404).send('Página não encontrada!');
+});
+
+server.listen(process.env.PORT)
 
