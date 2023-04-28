c190e9dca92b0993c6f155f9434428326eb6227d
“MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-04-28 11:09:48 -0300

    save

--- Torrent/src/controllers/filmsCategoryController.ts
+++ Torrent/src/controllers/filmsCategoryController.ts
@@ -1,6 +1,23 @@
 import { Request, Response} from "express";
+import { Category } from "../models/category";
 
 export const admin = async (req: Request, res: Response)=>{
 
     res.render('admin/categories/new')
+}
+
+export const save = async (req: Request, res: Response)=>{
+    let { name } = req.body
+
+    if(name){
+        let newCategory = new Category()
+
+        newCategory.name = name
+
+        await newCategory.save()
+    }else{
+        res.redirect('/admin/categories/new')
+    }
+
+    res.redirect("/admin/categories/index")
 }
--- Torrent/src/routes/filmsCategory.ts
+++ Torrent/src/routes/filmsCategory.ts
@@ -4,5 +4,6 @@ import * as filmsCategory from '../controllers/filmsCategoryController'
 const router = Router()
 
 router.get('/admin/categories/new', filmsCategory.admin)
+router.post('/categories/save', filmsCategory.save)
 
 export default router
