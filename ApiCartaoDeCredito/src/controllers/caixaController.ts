5820f9c9ef682daf86e76672d1102b347618f7b2
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-29 19:28:22 -0300

    api

--- ApiCartaoDeCredito/src/controllers/caixaController.ts
+++ ApiCartaoDeCredito/src/controllers/caixaController.ts
@@ -12,10 +12,10 @@ export const cartao = async(req:Request, res: Response)=>{
     let { id, nameDoTitular, numero, CVV, dataDeValidade } = req.body
 
     if(Caixa){
-        await Caixa.findAll({where:{id:id}})
+        await Caixa.findOne({where:{id:id}})
 
-        if(Caixa === numero.CVV){
-            res.status(400)
+        if(numero === id.numero){
+            res.sendStatus(400)
         }else{
 
             let newCartao = await Caixa.create({
@@ -30,13 +30,55 @@ export const cartao = async(req:Request, res: Response)=>{
 }
 
 export const idCartao = async(req: Request, res: Response)=>{
+    let id = req.params.id
 
+    if(isNaN(parseInt(id))){
+        res.sendStatus(400)
+    }else{
+        let id = parseInt(req.params.id)
+
+        let cartao = await Caixa.findOne({where:{id: id}})
+
+        if(cartao != undefined){
+            res.json({cartao})
+        }else{
+            res.sendStatus(404)
+        }
+    }
 }
 
 export const deleteCartao = async(req: Request, res: Response)=>{
+    let { id } = req.params
 
+    if(isNaN(parseInt(req.params.id))){
+        res.sendStatus(400)
+    }else{
+        await Caixa.destroy({
+            where:{id}
+        })
+
+        res.json({})
+    }
 }
 
 export const updateCartao = async(req: Request, res: Response)=>{
+    let {id} = req.params
+    let{ nameDoTitular, numero, CVV, dataDeValidade  } = req.body
 
+    if(isNaN(parseInt(req.params.id))){
+        res.sendStatus(400)
+    }else{
+        let mudarSeusDados = await Caixa.findByPk(id)
+
+        if(mudarSeusDados != undefined){
+            mudarSeusDados.nameDoTitular = nameDoTitular
+            mudarSeusDados.numero = numero
+            mudarSeusDados.CVV = CVV
+            mudarSeusDados.dataDeValidade = dataDeValidade
+
+            await mudarSeusDados.save()
+
+            res.json({mudarSeusDados})
+        }
+    }
 }
